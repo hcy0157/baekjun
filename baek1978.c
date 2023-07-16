784a11e0ccaf47781293506435f43742162cfcc0
hcy0157 <harnm0157@gmail.com>
2023-07-16 19:06:49 +0900

    23-07-16

--- baek1978.c
+++ baek1978.c
@@ -1,15 +1,38 @@
-//#define _CRT_SECURE_NO_WARNINGS
-//#include <stdio.h>
-//
-//int main() {
-//
-//
-//
-//
-//
-//
-//	printf("hello world");
-//
-//
-//	return 0;
-//}
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+int prime_number(int n);
+
+
+int prime_number(int n) {
+	if (n == 1)
+		return 0;
+
+	for (int i = 2; i < n; i++) {
+		if (n % i == 0)
+			return 0;
+	}
+
+
+	return 1;
+}
+
+
+int main() {
+
+	int N;
+	int sum = 0;
+	scanf("%d", &N);
+
+	for (int i = 0; i < N; i++) {
+		int number;
+		scanf("%d", &number);
+
+		sum += prime_number(number);
+	}
+
+	printf("%d", sum);
+	
+
+
+	return 0;
+}
